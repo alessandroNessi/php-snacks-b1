513b7bc806e18b4745d53d240561424c1f59fc58
alessandroNessi <alessandronessi@msn.com>
2021-10-19 16:21:27 +0200

    errore stampa squadre

--- snack_1.php
+++ snack_1.php
@@ -3,12 +3,17 @@ $nomi=["cippa","lippa","milano","genova","palermo","firenze","torino","livorno",
 //passo i nomi da estrapolare, il punteggio minimo e massimo da generare
 $squadre=populateTeam($nomi,10,90);
 
+//stampo le squadre in html
+for($i=0 ; $i < (count($squadre)) ; $i++){
+    echo "<p>{$squadre[$i]}</p>";
+}
+
+//genero e ritorno l'array con le squadre complete
 function populateTeam($nomi, $min, $max){
     $i=0;
     $positionTaken=[];
     $associatedPos=(int)rand($i+1,count($nomi)-1);
     $returningArray=[];
-    // echo (int)rand($i+1,count($nomi)-1);
     while(count($returningArray)<((int)(count($nomi)/2))){
         while(in_array($associatedPos, $positionTaken)){
             $associatedPos=(int)rand($i+1,count($nomi)-1);
@@ -16,7 +21,6 @@ function populateTeam($nomi, $min, $max){
         $returningArray[]=$nomi[$i] . " - " . $nomi[$associatedPos] . " | " . strval((int)rand($min,$max)) . "-" .strval((int)rand($min,$max));
         $positionTaken[]=$associatedPos;
         $positionTaken[]=$i;
-        echo var_dump($returningArray);
         $i++;
         while(in_array($i, $positionTaken)){
             $i++;
