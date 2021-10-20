75ac9f07884564115973fbb005a510dd7841eb66
alessandroNessi <alessandronessi@msn.com>
2021-10-20 12:32:38 +0200

    finish schifoso

---
+++ snack_6/index.php
@@ -0,0 +1,45 @@
+<?php
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach($db as $label => $objects){
+            echo "<div class='{$label}'>";
+            foreach($objects as $person){
+                echo "<p>{$person['name']} {$person['lastname']}</p>";
+            }
+            echo "</div>";
+        }
+    ?>
+</body>
+</html>
