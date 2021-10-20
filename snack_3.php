88279b55b9d10aac0eefcee4fc7a03d15106341e
alessandroNessi <alessandronessi@msn.com>
2021-10-20 11:50:29 +0200

    finito

---
+++ snack_3.php
@@ -0,0 +1,72 @@
+<?php
+    $posts = [
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+    // foreach ($posts as $date => $blogs){
+    //     foreach($blogs as $blog){
+
+    //     }
+    // }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>blog:</h1>
+    <ul>
+        <?php foreach($posts as $date => $blogs){
+            echo ("<li>{$date}</li><ul>");
+                foreach($blogs as $blog){
+                    echo ("<ul>");
+                    foreach($blog as $key => $value){
+                        // echo("<li><h3>{$value}</h3></li>");
+                        echo("<li><h3>{$value}</h3></li>");
+                    }   
+                    echo ("</ul>");
+                }
+            echo ("</ul>");
+        }
+        ?>
+    </ul>
+</body>
+</html>
