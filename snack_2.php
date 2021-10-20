c48a9034d4053f8dbdabd8d0fc17b69e37cd0882
alessandroNessi <alessandronessi@msn.com>
2021-10-20 10:51:26 +0200

    mi ero dimenticato eta e nome

--- snack_2.php
+++ snack_2.php
@@ -1,18 +1,46 @@
 <?php
-    $result="";
-    if(!empty($_GET["email"])){
-        $email=$_GET["email"];
-        if(strpos($email,"@")!=false 
-        && strpos($email,".")!=false 
-        && strpos($email,"@")<strpos($email,".")-1 
-        && strpos($email,"@")>0 
-        && strpos($email,".")<strlen($email)-1 
-        && substr_count($email,"@")<2 
-        && strlen($email)>4){
-            $result=true;
-        }else{
-            $result=false;
+    function emailOk() {
+        $result="";
+        if(!empty($_GET["email"])){
+            $email=$_GET["email"];
+            if(strpos($email,"@")!=false 
+            && strpos($email,".")!=false 
+            && strpos($email,"@")<strpos($email,".")-1 
+            && strpos($email,"@")>0 
+            && strpos($email,".")<strlen($email)-1 
+            && substr_count($email,"@")<2 
+            && strlen($email)>4){
+                $result=$email;
+            }else{
+                $result=false;
+            }
         }
+        return $result;
+    }
+    function nameOk() {
+        $result="";
+        if(!empty($_GET["name"])){
+            $name=$_GET["name"];
+            if(strlen($name)>3){
+                $result=$name;
+            }else{
+                $result=false;
+            }
+        }
+        return $result;
+    }
+    function ageOk() {
+        $result="";
+        if(!empty($_GET["age"])){
+            $age=$_GET["age"];
+            if(is_numeric($age)){
+                $result=$age;
+            }else{
+                $result=false;
+            }
+        }
+        // var_dump($result);
+        return $result;
     }
 ?>
 <!DOCTYPE html>
@@ -25,12 +53,17 @@
 </head>
 <body>
     <form action="./snack_2.php">
+        <input type="number" name="age" placeholder="Inserisci la tua età">
+        <input type="text" name="name" placeholder="Inserisci il tuo nome">
         <input type="email" name="email" placeholder="Inserisci la tua email">
         <input type="submit" value="Sbumit">
         <?php 
-            if($result===true){
-                echo "<h2>Login effettuato con successo, benvenuto {$email} .</h2>";
-            }elseif($result===false){
+            $email=emailOk();
+            $age=ageOk();
+            $name=nameOk();
+            if($email!=""&&$age!=""&&$name!=""){
+                echo "<h2>Benvenuto {$name}, età {$age}, email {$email} accesso effettuato con successo.</h2>";
+            }elseif(emailOk()===false){
                 echo "<h2>L'indirizzo di posta elettronica {$email} inserito non risulta corretto, ti preghiamo di inserire un indirizzo e-mail valido.</h2>";
             }
         ?>
