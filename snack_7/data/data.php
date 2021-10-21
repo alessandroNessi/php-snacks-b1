33b68271391f94e8b6ef2f446c6fd36433238945
alessandroNessi <alessandronessi@msn.com>
2021-10-21 12:29:54 +0200

    migliorato codice inclusi dati

---
+++ snack_7/data/data.php
@@ -0,0 +1,34 @@
+<?php 
+    $classe40=[
+        [
+            "nome"=> "alessandro",
+            "cognome"=> "nessi",
+            "voti"=> [5,6,7,8,9,10],
+        ],
+        [
+            "nome"=> "marco",
+            "cognome"=> "greg",
+            "voti"=> [5,6,7],
+        ],
+        [
+            "nome"=> "luca",
+            "cognome"=> "cattaneo",
+            "voti"=> [5,9,10],
+        ],
+        [
+            "nome"=> "gianni",
+            "cognome"=> "monaci",
+            "voti"=> [5,8,9],
+        ],
+        [
+            "nome"=> "jonny",
+            "cognome"=> "rudoph",
+            "voti"=> [5,6,0,0,0,0],
+        ],
+        [
+            "nome"=> "lillo",
+            "cognome"=> "weejee",
+            "voti"=> [1,2,3,4,5],
+        ],
+    ];
+?>
--- snack_7/index.php
+++ snack_7/index.php
@@ -1,36 +1,5 @@
 <?php
-    $classe40=[
-        [
-            "nome"=> "alessandro",
-            "cognome"=> "nessi",
-            "voti"=> [5,6,7,8,9,10],
-        ],
-        [
-            "nome"=> "marco",
-            "cognome"=> "greg",
-            "voti"=> [5,6,7],
-        ],
-        [
-            "nome"=> "luca",
-            "cognome"=> "cattaneo",
-            "voti"=> [5,9,10],
-        ],
-        [
-            "nome"=> "gianni",
-            "cognome"=> "monaci",
-            "voti"=> [5,8,9],
-        ],
-        [
-            "nome"=> "jonny",
-            "cognome"=> "rudoph",
-            "voti"=> [5,6,0,0,0,0],
-        ],
-        [
-            "nome"=> "lillo",
-            "cognome"=> "weejee",
-            "voti"=> [1,2,3,4,5],
-        ],
-    ];
+    include __DIR__ . '/data/data.php';
     function mediaVoti($array){
         $media=0;
         foreach($array as $element){
@@ -56,7 +25,7 @@
             echo "<div>
             <p>{$student['nome']} {$student['cognome']}</p>
             <p>Media voti: {$votomedio}</p>
-            </div>";
+            </div><hr>";
         }
     ?>
 </body>
